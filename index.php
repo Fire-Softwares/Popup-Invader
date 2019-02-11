e37bd70a0d923072dfb60a479fc56ffdb1ed93a7
Renaud <renaud.4243@gmail.com>
2019-02-11 20:17:59 +0100

    Commit

---
+++ index.php
@@ -0,0 +1,97 @@
+<!-- MIT License
+
+Copyright (c) 2019 Renaud, Fire-Softwares
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+-->
+<!-- POPUP-INVADER -->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <!-- Title of the page -->
+    <title>Popup Invader Demo</title>
+    <!-- Fire-API Minified CSS -->
+    <link rel='stylesheet' type='text/css' href='https://cdn.fire-softwares.ga/fire-api/web-framework/css/fire-api.min.css'/>
+    <!-- Main stylesheet -->
+    <style>
+      h2 {
+        margin-top: 21.5%;                         /* Center vertically */
+        text-align: center;                        /* Center horizontally */
+      }
+      .ok {
+        font-size: 32px;                           /* Increasing font size */
+        margin-left: calc(50% - 35px);             /* Center horizontally */
+      }
+      .source {
+        font-size: 32px;                           /* Increasing font size */
+        margin-left: calc(50% - 88px);             /* Center horizontally */
+      }
+    </style>
+  </head>
+
+<?php
+  // If user confirmed to join the website
+  if (isset($_GET["confirm"]) && $_GET["confirm"] == "yes")
+    setcookie("confirm-popup-invader", "yes", time() + 86400);  // One day cookie
+
+  // If user haven't already confirmed to join the website
+  if (!isset($_COOKIE["confirm-popup-invader"]) || !(isset($_GET["confirm"]) && $_GET["confirm"] == "yes")):
+?>
+  <body>
+    <!-- Just a warning -->
+    <h2>Warning : this website can harm your web browser.<br/>
+    Click "OK" to confirm you know what you're doing.</h2><br/>
+    <!-- Just a button who can destroy your life -->
+    <a class="light-red hypertext ok" href="?confirm=yes">OK</a>
+  </body>
+<?php
+  else:
+?>
+  <body>
+    <!-- Just an information -->
+    <h2>If nothing happens, try allow popups :)</h2><br/>
+    <!-- Just another information -->
+    <a class="light-red hypertext source" href="https://github.com/Fire-Softwares/Popup-Invader" target="_blank">Source code</a>
+    <!-- Fire-Softwares Popup Invader script invoke -->
+    <script src='popup-invader.js'></script>
+    <!-- Background wave -->
+    <script>
+      var x1 = .0, x2 = .25, x3 = .5; // x Sinus
+      var red, green, blue;           // RGB vars
+
+      setInterval(function() {
+        red = Math.sin(x1);
+        green = Math.sin(x2);
+        blue = Math.sin(x3);
+
+        x1 += .04;
+        x2 += .02;
+        x3 += .01;
+
+        document.body.style.backgroundColor = "rgb(" + (191 + red * 64)  + ", " + (191 + green * 64) + ", " + (191 + blue * 64) + ")";
+      }, 30);
+    </script>
+    <!-- Fire-API Minified JavaScript -->
+    <script src='https://cdn.fire-softwares.ga/fire-api/web-framework/js/fire-api.min.js'></script>
+  </body>
+<?php
+  endif;
+?>
+</html>
---
+++ popup-invader.js
@@ -0,0 +1,59 @@
+/* MIT License
+
+Copyright (c) 2019 Renaud, Fire-Softwares
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/**************************************************************************
+ *           .______     ______   .______    __    __  .______            *
+ *          |   _  \   /  __  \  |   _  \  |  |  |  | |   _  \            *
+ *          |  |_)  | |  |  |  | |  |_)  | |  |  |  | |  |_)  |           *
+ *          |   ___/  |  |  |  | |   ___/  |  |  |  | |   ___/            *
+ *          |  |      |  `--'  | |  |      |  `--'  | |  |                *
+ *          | _|       \______/  | _|       \______/  | _|                *
+ *                                                                        *
+ *  __  .__   __. ____    ____  ___       _______   _______ .______       *
+ * |  | |  \ |  | \   \  /   / /   \     |       \ |   ____||   _  \      *
+ * |  | |   \|  |  \   \/   / /  ^  \    |  .--.  ||  |__   |  |_)  |     *
+ * |  | |  . `  |   \      / /  /_\  \   |  |  |  ||   __|  |      /      *
+ * |  | |  |\   |    \    / /  _____  \  |  '--'  ||  |____ |  |\  \----. *
+ * |__| |__| \__|     \__/ /__/     \__\ |_______/ |_______|| _| `._____| *
+ **************************************************************************/
+
+
+/**************************
+*         SETTINGS        *
+**************************/
+var website = "https://cdn.fire-softwares.ga/popup-invader/?confirm=yes"; // Website that the pop-ups will browse to
+                                                                          // /!\ Redirecting to the website itself
+                                                                          // will make it replicate
+var title   = "Popup Invader by Fire-Softwares";                          // Title of the pop-up
+var size    = [200, 200];                                                 // Size of pop-ups ([width, height])
+var delay   = 2;                                                          // Delay (in milliseconds) between each pop-up
+
+/**************************
+*        MAIN CODE        *
+**************************/
+setInterval(function() {  // Make script run each *delay* milliseconds
+  window.open(website, title, "height=" + size[1]                                                         // Setting pop-up height to size[1]
+                            + ",width=" + size[0]                                                         // Setting pop-up width to size[0]
+                            + ",top=" + Math.floor(Math.random() * Math.floor(screen.height - size[1]))   // Setting window y to a random location in [0 ; clientHeight - popupHeight]
+                            + ",left=" + Math.floor(Math.random() * Math.floor(screen.width - size[0]))); // Setting window x to a random location in [0 ; clientWidth - popupWidth]
+}, delay);
